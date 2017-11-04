d7741180f3a0e89a14ac5d8ea2a9a79869ceb924
Максим Ильин <negamaxi@gmail.com>
2017-11-04 15:06:30 +0300

    root component renamed to Root

--- app/index.js
+++ app/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import App from './view/App';
+import Root from './view/Root';
 import './css/izi/index.global.scss';
 import './css/index.global.scss';
 
 render(
   <AppContainer>
-    <App />
+    <Root />
   </AppContainer>,
   document.getElementById('root')
 );
 
 if (module.hot) {
-  module.hot.accept('./view/App', () => {
-    const NextApp = require('./view/App'); // eslint-disable-line global-require
+  module.hot.accept('./view/Root', () => {
+    const NextRoot = require('./view/Root'); // eslint-disable-line global-require
     render(
       <AppContainer>
-        <NextApp />
+        <NextRoot />
       </AppContainer>,
       document.getElementById('root')
     );
---
+++ app/view/Root.js
@@ -0,0 +1,15 @@
+
+// @flow
+import React from 'react';
+import Loader from './Loader';
+import App from './App';
+
+// import InitApp from './InitApp/index';
+
+const Root = () => (
+  <div className="izi-vw-scale-factor-5 izi--adaptive izi-fill">
+    <App />
+  </div>
+);
+
+export default Root;
