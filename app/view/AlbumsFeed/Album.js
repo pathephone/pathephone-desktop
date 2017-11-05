4dda636d6c26e222fe42e5be2a22fdb1918cfb63
Максим Ильин <negamaxi@gmail.com>
2017-11-05 13:14:04 +0300

    Album component enhanced markup + css

--- app/view/AlbumsFeed/Album.js
+++ app/view/AlbumsFeed/Album.js
@@ -5,12 +5,16 @@ const Album = (album) => {
   const { title, artist, coverURL, _id } = album
   return (
     <div className='album' key={_id}>
-      <div className='album_play-button'>
-        {
-        coverURL
-      ? <img src={coverURL} className='album_cover' />
-      : <img src={noCoverImg} className='album-cover' />
-      }
+      <div
+        className='album_cover-area izi-yr'
+        style={{
+          background: `url(${coverURL || noCoverImg})`,
+          backgroundSize: 'cover'
+        }}
+      >
+        <button className='album_play-button izi-margin-top-auto' onClick={() => alert(_id)}>
+          >
+        </button>
       </div>
       <h4 className='album_title'>{title}</h4>
       <h5 className='album_artist'>{artist}</h5>
