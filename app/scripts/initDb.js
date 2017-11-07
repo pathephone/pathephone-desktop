d95aafb889e99dadd77e35537345c9af46313266
Максим Ильин <negamaxi@gmail.com>
2017-11-07 15:55:30 +0300

    inital scripts separeted, cleaned

--- app/scripts/initDb.js
+++ app/scripts/initDb.js
@@ -1,44 +1,13 @@
 import rxdbAlbumSchema from '../schemas/rxdb/album'
-import metabinAlbumSchema from '../schemas/metabin/album'
-import { getIpfs } from '../api/ipfs'
-import { getDb, createDb } from '../api/rxdb'
-import getCidString from '../utils/getCidString'
+import getDb, { createDb } from '../api/rxdb'
 
-const dagParams = { format: 'dag-cbor', hashAlg: 'sha3-512' }
-
-export const start = async () => {
-  const IPFSnode = getIpfs()
+const initIpfs = async () => {
   await createDb({
     name: 'pathephone',
     adapter: 'idb'
   })
   const db = getDb()
-  const promises = [rxdbAlbumSchema].map(
-    async (schema) => {
-      const { collectionName, schemaObj } = schema
-      const collection = await db.collection({
-        name: collectionName,
-        schema: schemaObj
-      })
-      if (!metabinAlbumSchema.schemaCid) {
-        const cidObj = await IPFSnode.dag.put(metabinAlbumSchema.schemaObj, dagParams)
-        metabinAlbumSchema.schemaCid = cidObj.toBaseEncodedString()
-      }
-      await IPFSnode.pubsub.subscribe(
-        metabinAlbumSchema.schemaCid,
-        async (message) => {
-          try {
-            const { data } = message
-            const cidString = getCidString(data)
-            console.log(`Album candidate received: ${cidString}`)
-            const { value } = await IPFSnode.dag.get(cidString)
-            collection.insert({ cid: cidString, data: value })
-          } catch (e) {
-            console.log(e)
-          }
-        }
-      )
-    }
-  )
-  return Promise.all(promises)
+  await db.collection(rxdbAlbumSchema)
 }
+
+export default initIpfs
---
+++ app/scripts/initIpfs.js
@@ -0,0 +1,31 @@
+import rxdbAlbumSchema from '../schemas/rxdb/album'
+import getIpfs from '../api/ipfs'
+import getDb from '../api/rxdb'
+import getCidString from '../utils/getCidString'
+
+const dagParams = { format: 'dag-cbor', hashAlg: 'sha3-512' }
+
+const initIpfs = async () => {
+  const IPFSnode = getIpfs()
+  const { albums } = getDb()
+  const { schema } = rxdbAlbumSchema
+  const cidObj = await IPFSnode.dag.put(schema, dagParams)
+  const cidString = cidObj.toBaseEncodedString()
+  console.log('INITIALISING LISTENERS')
+  await IPFSnode.pubsub.subscribe(
+    cidString,
+    async (message) => {
+      try {
+        const { data } = message
+        const cidString = getCidString(data)
+        console.log(`Album candidate received: ${cidString}`)
+        const { value } = await IPFSnode.dag.get(cidString)
+        albums.insert({ cid: cidString, data: value })
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  )
+}
+
+export default initIpfs
