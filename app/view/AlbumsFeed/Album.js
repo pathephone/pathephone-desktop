82a71d87aed9eac5f2c98a62b8783fee6ae66de0
Максим Ильин <negamaxi@gmail.com>
2017-11-14 22:44:05 +0300

    enhanced Album component

--- app/view/AlbumsFeed/Album.js
+++ app/view/AlbumsFeed/Album.js
@@ -1,30 +1,42 @@
 import React from 'react'
 import noCoverImg from './no-cover.png'
 import playTracks from 'scripts/playTracks'
+import Async from '../_/Async'
+import multihashToUrl from 'scripts/multihashToUrl'
+import MdPlay from 'react-icons/lib/md/play-arrow'
 
 const Album = (album) => {
-  const { title, artist, coverURL, id, tracks } = album
+  const { cid, data } = album
+  const { title, artist, cover, tracks } = data
   const playThisAlbum = () => {
     playTracks(tracks)
   }
-  return (
-    <div className='album' key={id}>
+  const ReadyView = ({ data }) => (
+    <div className='album'>
       <div
         className='album_cover-area izi-yr'
         style={{
-          background: `url(${coverURL || noCoverImg})`,
+          background: `url(${data})`,
           backgroundSize: 'cover'
         }}
       >
         <button
           className='album_play-button izi-margin-top-auto'
-          onClick={playThisAlbum}>
-          >
+          onClick={playThisAlbum}
+        >
+          <MdPlay />
         </button>
       </div>
       <h4 className='album_title'>{title}</h4>
       <h5 className='album_artist'>{artist}</h5>
     </div>
+  )
+  return (
+    <Async
+      key={cid}
+      call={() => multihashToUrl(cover)}
+      readyView={ReadyView}
+    />
   )
 }
 
