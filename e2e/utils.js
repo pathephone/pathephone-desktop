11185ce33885d2650dce6ca36018008372e7a57e
Alexey Kasyanchuk <degitx@gmail.com>
2017-11-22 23:34:44 +0300

    timeout fix

--- e2e/utils.js
+++ e2e/utils.js
@@ -2,18 +2,18 @@ import electron from "electron";
 import { Application } from "spectron";
 
 const beforeEach = function() {
-  this.timeout(10000);
+  this.timeout(20000);
   this.app = new Application({
     path: electron,
     args: ["."],
-    startTimeout: 10000,
-    waitTimeout: 10000
+    startTimeout: 20000,
+    waitTimeout: 20000
   });
   return this.app.start();
 };
 
 const afterEach = function() {
-  this.timeout(10000);
+  this.timeout(20000);
   if (this.app && this.app.isRunning()) {
     return this.app.stop();
   }
