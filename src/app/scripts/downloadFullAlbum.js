369d7283774fb6ca98642db177696ced9501c262
Alexey Kasyanchuk <degitx@gmail.com>
2017-12-15 06:07:14 +0300

    ignore current track on album downloading: no necessary download

--- src/app/scripts/downloadFullAlbum.js
+++ src/app/scripts/downloadFullAlbum.js
@@ -1,7 +1,10 @@
 import getIpfsNode from '../api/ipfs'
 
-export default async (playlist, index) => {
+export default async (playlist, ignoreIndex) => {
   const ipfsNode = getIpfsNode()
+  if(typeof ignoreIndex !== 'undefined') {
+    playlist = playlist.filter((song, idx) => idx !== ignoreIndex) 
+  }
   let hashes = playlist.map(song => song.hash)
   const existsHashes = await ipfsNode.files.exists(hashes)
   hashes = hashes.filter(el => existsHashes.indexOf(el) === -1)
