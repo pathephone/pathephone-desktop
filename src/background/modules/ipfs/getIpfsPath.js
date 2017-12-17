4056938bd14a0062f41dbcd16b2e4550eff689b2
Alexey Kasyanchuk <degitx@gmail.com>
2017-12-17 23:05:56 +0300

    Fixed Mac OS bug #50

--- src/background/modules/ipfs/getIpfsPath.js
+++ src/background/modules/ipfs/getIpfsPath.js
@@ -18,6 +18,10 @@ const getRelativeIpfsPath = () => {
     return fs.realpathSync(path.join(__dirname, '/../../..')) + '/ipfs'
   }
 
+  if (process.platform === 'darwin' && fs.existsSync(fs.realpathSync(path.join(__dirname, '/../../../MacOS')) + '/ipfs')) {
+    return fs.realpathSync(path.join(__dirname, '/../../../MacOS')) + '/ipfs'
+  }
+
   if (/^win/.test(process.platform) && fs.existsSync('imports/win/ipfs.exe')) {
     return 'imports/win/ipfs.exe'
   }
