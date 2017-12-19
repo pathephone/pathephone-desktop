bdce7bcbfc8d11ff872aa4154aeaa7e3f60056fe
Максим Ильин <negamaxi@gmail.com>
2017-12-20 00:34:43 +0300

    Navigation styling moved to jsx

--- src/app/view/Navigation.js
+++ src/app/view/Navigation.js
@@ -1,28 +1,19 @@
 import React, { Component } from 'react'
 import pageState from '../state/page'
 import bind from '../utils/recallReact'
-import pagesMap from './pagesMap'
 import AddAlbum from './AddAlbum'
 
 class Navigation extends Component {
   render () {
-    const { page } = this.props
     return (
-      <nav className='navigation izi-ys'>
-        {
-          pagesMap.map(({ name, title }) => (
-            <button
-              key={name}
-              className={`navigation_item ${name === page.name ? 'active' : ''}`}
-              onClick={
-                () => pageState('CHANGE', name)
-              }
-            >
-              {title}
-            </button>
-          ))
-        }
+      <nav className='navigation izi-ys izi-padding'>
         <AddAlbum />
+        <style jsx>{`
+
+.navigation {
+  background-color: darkslategrey;
+}
+        `}</style>
       </nav>
     )
   }
