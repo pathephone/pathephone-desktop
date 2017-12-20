8977676f34a05b327bcc5594ba2083a1c85f6347
Максим Ильин <negamaxi@gmail.com>
2017-12-20 19:49:03 +0300

    static AudioElement instead of CID input

--- src/app/view/AddAlbum/FormAlbum/TrackInput.js
+++ src/app/view/AddAlbum/FormAlbum/TrackInput.js
@@ -1,5 +1,25 @@
 import React from 'react'
 import Input from '../../_/Input'
+import MdAudio from 'react-icons/lib/md/audiotrack'
+
+const AudioElement = ({ hash }) => (
+  <div className='izi-x'>
+    <MdAudio className='icon' />
+    <span className='hash izi-dots izi-margin-left'>
+      {hash}
+    </span>
+    <style jsx>{`
+.icon {
+  flex-shrink: 0
+}
+.hash {
+  white-space: nowrap;
+  font-size: 0.75em;
+  color: #434343;
+}
+    `}</style>
+  </div>
+)
 
 class TrackInput extends React.Component {
   render () {
@@ -7,31 +27,34 @@ class TrackInput extends React.Component {
     const { title, hash, artist } = value
     return (
       <p className='izi-ys' style={{ flexBasis: '100%' }}>
-        <Input
-          value={title}
-          name='title'
-          placeholder='Track title'
-          label
-          type='text'
-          onChange={onChange}
-        />
+        <div className='izi-x'>
+          <div className='izi-yl izi-fill-width'>
+            <Input
+              className='izi-fill-width'
+              value={title}
+              name='title'
+              placeholder='Track title'
+              label
+              type='text'
+              onChange={onChange}
+            />
+          </div>
+          <br />
+          <div className='izi-yl izi-fill-width izi-margin-left'>
+            <Input
+              className='izi-fill-width'
+              value={artist}
+              name='artist'
+              label
+              placeholder='Artist'
+              type='text'
+              onChange={onChange}
+            />
+          </div>
+        </div>
         <br />
-        <Input
-          value={artist}
-          name='artist'
-          label
-          placeholder='Artist'
-          type='text'
-          onChange={onChange}
-        />
-        <br />
-        <Input
-          value={hash}
-          name='hash'
-          label
-          placeholder='Audio data CID'
-          type='text'
-          onChange={onChange}
+        <AudioElement
+          hash={hash}
         />
       </p>
     )
