1ef4bfc6a18e0693d11610837ed8590ca63b36ac
Максим Ильин <negamaxi@gmail.com>
2017-12-21 15:09:56 +0300

    wierd bugfix, must be investigated

--- src/app/scripts/multihashToUrl.js
+++ src/app/scripts/multihashToUrl.js
@@ -1,7 +1,13 @@
 import streamToBlob from './streamToBlob'
 import multihashToStream from './multihashToStream'
+import { setTimeout } from 'core-js/library/web/timers'
+
+const asyncTimeout = (timeout) => new Promise(
+  resolve => setTimeout(resolve, timeout)
+)
 
 const multihashToUrl = async (multihash) => {
+  await asyncTimeout(1000)
   const stream = await multihashToStream(multihash)
   if (Buffer.isBuffer(stream)) {
     return URL.createObjectURL(new Blob([stream], {type: 'application/octet-stream'}))
