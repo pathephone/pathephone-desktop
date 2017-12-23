fb38e691ae1e26910653f1007630ea084f57160a
Максим Ильин <negamaxi@gmail.com>
2017-12-23 13:18:45 +0300

    new tests + assets

--- e2e/tests/add-album.js
+++ e2e/tests/add-album.js
@@ -1,5 +1,6 @@
 /* eslint-env mocha */
-import utils from '../utils'
+const utils = require('../utils')
+const path = require('path')
 
 describe('add album process', function () {
   this.timeout(30000)
@@ -8,12 +9,42 @@ describe('add album process', function () {
   describe('click add album button', () => {
     it('should throw no errors', async function () {
       const { app } = this
-      await app.client.waitForExist('#add-album_button')
-      return app.client.click('#add-album_button')
+      await app.client.waitForExist('#add-album_open')
+      return app.client.click('#add-album_open')
     })
-    it('form should appear', function () {
+    it('add album form should appear', function () {
       const { app } = this
       return app.client.waitForExist('#add-album_form')
+    })
+  })
+  describe('click add album submit button', () => {
+    it('should throw no errors', async function () {
+      const { app } = this
+      await app.client.waitForExist('#add-album_submit')
+      return app.client.click('#add-album_submit')
+    })
+    it('errors fieldset should appear', function () {
+      const { app } = this
+      return app.client.waitForExist('#add-album_errors')
+    })
+  })
+  describe('add tracks manually', () => {
+    it('should throw no errors', async function () {
+      const { app } = this
+      const testFilePath = path.join(__dirname, 'add-album/test-track.mp3')
+      return app.client.chooseFile('#add-album_add-tracks', testFilePath)
+    })
+    it('should fill album.title data', async function () {
+      const { app } = this
+      const value = await app.client.getValue('#input_album-title]')
+      console.log(value)
+      if (value !== 'Red Flower') {
+        throw new Error('Value does not match.')
+      }
+    })
+    it('fieldset should appear', function () {
+      // const { app } = this
+      // return app.client.waitForExist('#add-album_errors')
     })
   })
 })
