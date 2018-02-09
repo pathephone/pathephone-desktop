6acde7cf4469cd278488fbf9dcc22fb2d9d9bc3f
Максим Ильин <negamaxi@gmail.com>
2018-02-09 10:49:30 +0300

    fix(start-screen): missing errors (#135)

--- src/app/view/InitApp/StartScreen.js
+++ src/app/view/InitApp/StartScreen.js
@@ -5,15 +5,15 @@ import ErrorView from './StartScreen/ErrorView'
 
 import './StartScreen/StartScreen.css'
 
-const StartScreen = ({ stage, payload }) => {
+const StartScreen = ({ stage, message, error }) => {
   const finalStage = 2
   const progress = stage / finalStage * 100
   return (
     <div className='start-screen izi-fill izi-middle izi-y'>
-      <ProgressBar progress={progress} message={payload && payload.message} />
+      <ProgressBar progress={progress} message={message} />
       {
-        payload && payload.error && (
-          <ErrorView error={payload.error} />
+        error && (
+          <ErrorView error={error} />
         )
       }
     </div>
--- src/app/view/InitApp/StartScreen/ErrorView.js
+++ src/app/view/InitApp/StartScreen/ErrorView.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const ErrorView = ({ error }) => {
-  return <h4 className='izi-red izi-uppercase'>{error.message}</h4>
+  return <h4 className='izi-red izi-uppercase izi-text-center'>{error.message}</h4>
 }
 
 export default ErrorView
