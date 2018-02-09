40a4f98f608119316c583df4f7c19da2be3b95db
Максим Ильин <negamaxi@gmail.com>
2018-02-10 00:46:08 +0300

    actual changes + travis deploy for master only

--- src/app/view/AlbumsFound.js
+++ src/app/view/AlbumsFound.js
@@ -2,14 +2,19 @@ import React from 'react'
 import AlbumsFeed from './AlbumsFeed'
 import Rxdb from './_/Rxdb'
 
+const NoAlbumsFound = () => (
+  <div className='izi--gap izi-y izi-center izi-fill'>
+    <h4 className='izi-gray izi-uppercase'>no albums yet</h4>
+    <p className='izi-text-center'>Albums will appear gradually, as they are discovered. Also you can add your own albums to the feed, which will makes them available to other members of the network too.</p>
+  </div>
+)
+
 const AlbumsFound = ({ data, error }) => {
   if (error) {
     return <h1>{error.message}</h1>
   }
   if (!data || data.length === 0) {
-    return (
-      <h1>No albums found yet</h1>
-    )
+    return <NoAlbumsFound />
   }
   return <AlbumsFeed albums={data} />
 }
