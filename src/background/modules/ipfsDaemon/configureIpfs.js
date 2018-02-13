91c3a431e2b2af89bed6605d91e84aa7be6428e5
Alexey Kasyanchuk <degitx@gmail.com>
2018-02-13 14:31:25 +0300

    fix(macOS): unable to resolve path with spaces (#144)
    Fixed #143

--- src/background/modules/ipfsDaemon/configureIpfs.js
+++ src/background/modules/ipfsDaemon/configureIpfs.js
@@ -4,12 +4,12 @@ const exec = util.promisify(require('child_process').exec)
 
 export default async (options, ipfsPath) => {
   if (options.port) {
-    await exec(`${ipfsPath} config --json Addresses.Swarm "[\\"/ip4/0.0.0.0/tcp/${options.port}\\", \\"/ip6/::/tcp/${options.port}\\"]"`, options)
+    await exec(`"${ipfsPath}" config --json Addresses.Swarm "[\\"/ip4/0.0.0.0/tcp/${options.port}\\", \\"/ip6/::/tcp/${options.port}\\"]"`, options)
   }
   if (options.portApi) {
-    await exec(`${ipfsPath} config Addresses.API "/ip4/127.0.0.1/tcp/${options.portApi}"`, options)
+    await exec(`"${ipfsPath}" config Addresses.API "/ip4/127.0.0.1/tcp/${options.portApi}"`, options)
   }
   if (options.portGateway) {
-    await exec(`${ipfsPath} config Addresses.Gateway "/ip4/127.0.0.1/tcp/${options.portGateway}"`, options)
+    await exec(`"${ipfsPath}" config Addresses.Gateway "/ip4/127.0.0.1/tcp/${options.portGateway}"`, options)
   }
 }
