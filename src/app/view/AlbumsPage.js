8b5f56f4865bb4ede47e89825d2d37044102c285
Максим Ильин <negamaxi@gmail.com>
2018-02-15 13:53:23 +0300

    search improvements

--- src/app/view/AlbumsPage.js
+++ src/app/view/AlbumsPage.js
@@ -12,23 +12,41 @@ const NoAlbumsFound = () => (
   </div>
 )
 
+const getQueryFields = ($regex) => [
+  {
+    'data.artist': { $regex }
+  },
+  {
+    'data.title': { $regex }
+  }
+
+]
+
 const createAlbumsQuery = (searchText) => {
   if (searchText) {
-    const expression = new RegExp(searchText, 'i')
-    const orQuery = [
-      {
-        'data.artist': {
-          $regex: expression
-        }
-      },
-      {
-        'data.title': {
-          $regex: expression
+    const words = searchText.split(' ')
+    const handleFilter = s => s
+    const pureWords = words.filter(handleFilter)
+    if (pureWords.length === 1) {
+      const regex = new RegExp(pureWords[0], 'i')
+      return {
+        $or: getQueryFields(regex)
+      }
+    } else
+    if (pureWords.length > 1) {
+      let expression = '('
+      const handleEach = (word, index, array) => {
+        if (index === array.length - 1) {
+          expression = expression.concat(word, ')')
+        } else {
+          expression = expression.concat(word, '|')
         }
       }
-    ]
-    return {
-      $or: orQuery
+      pureWords.forEach(handleEach)
+      const regex = new RegExp(expression, 'i')
+      return {
+        $and: getQueryFields(regex)
+      }
     }
   }
 }
