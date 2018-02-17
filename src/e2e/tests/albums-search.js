28a58155357b6ec88064789be5d113f1856c04c2
Максим Ильин <negamaxi@gmail.com>
2018-02-17 12:04:21 +0300

    attempt to fix search tests

--- src/e2e/tests/albums-search.js
+++ src/e2e/tests/albums-search.js
@@ -15,9 +15,10 @@ describe('albums search', function () {
     })
     it('no albums on the feed', async function () {
       const { app } = this
-      await app.client.waitForExist('#albums-feed')
-      const albums = await app.client.$$('#albums-feed .album')
-      expect(albums.length).to.be.equal(0)
+      await app.client.waitUntil(async () => {
+        const albums = await app.client.$$('#albums-feed .album')
+        return albums.length === 0
+      })
     })
   })
   describe('click cancel search button', function () {
@@ -28,11 +29,10 @@ describe('albums search', function () {
     })
     it('input is empty', async function () {
       const { app } = this
-      await app.client.waitForExist(inputSelector)
       const value = await app.client.$(inputSelector).getValue()
       expect(value).to.equal('')
     })
-    it('albums appears on the feed', async function () {
+    it('albums appear on the feed', async function () {
       const { app } = this
       await app.client.waitForExist('#albums-feed .album')
       const albums = await app.client.$$('#albums-feed .album')
