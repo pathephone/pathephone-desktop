6114db9e5b1f1c07e541d2957995064a9ec5f4ba
Максим Ильин <negamaxi@gmail.com>
2018-02-27 11:09:43 +0300

    e2e development testing

--- src/e2e/utils.js
+++ src/e2e/utils.js
@@ -4,14 +4,18 @@ const { platform } = process
 
 let pathToBin
 
-if (platform === 'darwin') {
-  pathToBin = 'dist/mac/Pathephone.app/Contents/MacOS/Pathephone'
-} else
-if (platform === 'linux') {
-  pathToBin = 'dist/linux-unpacked/pathephone-desktop'
-} else
-if (platform === 'win32') {
-  pathToBin = 'dist/win-unpacked/Pathephone.exe'
+if (process.env.E2E_TYPE === 'dev') {
+  pathToBin = require('electron')
+} else {
+  if (platform === 'darwin') {
+    pathToBin = 'dist/mac/Pathephone.app/Contents/MacOS/Pathephone'
+  } else
+  if (platform === 'linux') {
+    pathToBin = 'dist/linux-unpacked/pathephone-desktop'
+  } else
+  if (platform === 'win32') {
+    pathToBin = 'dist/win-unpacked/Pathephone.exe'
+  }
 }
 
 const startApp = async function () {
