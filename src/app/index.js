62b845a0cd28ea0478a1403767604a5e0b1fc73a
Максим Ильин <negamaxi@gmail.com>
2018-03-26 00:20:56 +0300

    provider

--- src/app/index.js
+++ src/app/index.js
@@ -5,9 +5,16 @@ import './css/animate.css'
 
 import React from 'react'
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
 import Root from './view/Root'
 
+const store = createStore(rootReducer)
+
 render(
-  <Root />,
+  <Provider store={store}>
+    <Root />
+  </Provider>,
   document.getElementById('mount-point')
 )
