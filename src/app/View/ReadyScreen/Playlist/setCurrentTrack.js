073915e4825a58b5714d346dd5f7baa876eb7870
negamaxi <negamaxi@gmail.com>
2018-04-04 21:31:21 +0300

    removed old app folder

--- src/app/View/ReadyScreen/Playlist/setCurrentTrack.js
+++
@@ -1,7 +0,0 @@
-import playlistState from '~/state/playlist'
-
-const setCurrentTrack = (id) => {
-  playlistState('SET_CURRENT', id)
-}
-
-export default setCurrentTrack
--- src/app/View/Root.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Root = ({ children }) => (
-  <div id='root' className='izi-vw-scale-factor-5 izi-fill izi--adaptive'>
-    {children}
-  </div>
-)
-
-export default Root
