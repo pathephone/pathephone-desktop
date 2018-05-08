c6d2ad8f2d75bdc8be2e5682070d64333e09d8f5
Максим Ильин <maxim.iljin@oxagile.com>
2018-05-08 16:19:24 +0300

    icons for navigation menu

--- src/renderer/view/App/ReadyScreen/Navigation.jsx
+++ src/renderer/view/App/ReadyScreen/Navigation.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+import UploadIcon from 'react-icons/lib/md/cloud-upload'
+import LibraryIcon from 'react-icons/lib/md/library-music'
+
 import { ROUTE_ADD_ALBUM, ROUTE_ALBUMS } from '~data/constants'
 
 import NavigationItem from './Navigation/NavigationItem.jsx'
@@ -11,10 +14,12 @@ const Navigation = () => (
     <NavigationItem
       path={ROUTE_ALBUMS}
       title='Albums found'
+      icon={<LibraryIcon />}
     />
     <NavigationItem
       path={ROUTE_ADD_ALBUM}
-      title='Add album'
+      title='Share album'
+      icon={<UploadIcon />}
     />
   </nav>
 )
--- src/renderer/view/App/ReadyScreen/Navigation/NavigationItem.jsx
+++ src/renderer/view/App/ReadyScreen/Navigation/NavigationItem.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import propTypes from 'prop-types'
 import { NavLink } from 'react-router-dom'
 
-const NavigationItem = ({ path, title }) => (
+const NavigationItem = ({ path, title, icon }) => (
   <NavLink to={path} className='navigation__item' activeClassName='navigation__item--active'>
+    {icon}
     {title}
   </NavLink>
 )
 
 NavigationItem.propTypes = {
   path: propTypes.string.isRequired,
-  title: propTypes.string.isRequired
+  title: propTypes.string.isRequired,
+  icon: propTypes.object.isRequired
 }
 
 export default NavigationItem
