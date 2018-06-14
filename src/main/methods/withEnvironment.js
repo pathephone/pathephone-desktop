6140ad3958695d70f0cf0f3a5798446e99ac8652
Максим Ильин <negamaxi@gmail.com>
2018-06-14 16:54:48 +0300

    brings non-production bugfix

--- src/main/methods/withEnvironment.js
+++ src/main/methods/withEnvironment.js
@@ -4,7 +4,7 @@ import jetpack from 'fs-jetpack'
 import { ENVIRONMENT, IS_PRODUCTION, IS_TESTING, IS_DEVELOPMENT } from '#config'
 
 const withEnvronment = () => {
-  if (IS_PRODUCTION) {
+  if (!IS_PRODUCTION) {
     const userDataPath = app.getPath('userData')
     const nextDataPath = `${userDataPath} (${ENVIRONMENT})`
     if (IS_TESTING) {
