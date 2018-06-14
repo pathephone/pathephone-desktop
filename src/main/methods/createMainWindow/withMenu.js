03d80e2d669591eb0d50ec88664cf08d85f316a7
Максим Ильин <negamaxi@gmail.com>
2018-06-14 18:21:42 +0300

    brings development menu

--- src/main/methods/createMainWindow/withMenu.js
+++ src/main/methods/createMainWindow/withMenu.js
@@ -1,8 +1,37 @@
-import { IS_DEVELOPMENT } from '#config'
+import { BrowserWindow, Menu, app } from 'electron'
+
+import { IS_PRODUCTION } from '#config'
+
+const devMenuTemplate = {
+  label: 'Development',
+  submenu: [
+    {
+      label: 'Reload',
+      accelerator: 'CmdOrCtrl+R',
+      click: () => {
+        BrowserWindow.getFocusedWindow().webContents.reloadIgnoringCache()
+      }
+    },
+    {
+      label: 'Toggle DevTools',
+      accelerator: 'Alt+CmdOrCtrl+I',
+      click: () => {
+        BrowserWindow.getFocusedWindow().toggleDevTools()
+      }
+    },
+    {
+      label: 'Quit',
+      accelerator: 'CmdOrCtrl+Q',
+      click: () => {
+        app.quit()
+      }
+    }
+  ]
+}
 
 const withMenu = window => {
-  if (!IS_DEVELOPMENT) {
-    window.setMenu(null)
+  if (!IS_PRODUCTION) {
+    window.setMenu(Menu.buildFromTemplate([devMenuTemplate]))
   }
 }
 
