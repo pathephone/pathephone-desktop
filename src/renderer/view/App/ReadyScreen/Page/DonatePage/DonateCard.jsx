78946b9f6bde239f84e95aa734634ba8177fd7e6
Максим Ильин <negamaxi@gmail.com>
2018-07-03 21:47:24 +0300

    fix():  add yandex donate button to about page

--- src/renderer/view/App/ReadyScreen/Page/DonatePage/DonateCard.jsx
+++ src/renderer/view/App/ReadyScreen/Page/DonatePage/DonateCard.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
 
 import suatmm from './DonateCard/suatmm.gif'
-import DonatePill from './DonateCard/DonatePill.jsx'
 
 import './DonateCard.css'
 
 const DonateCard = () => (
   <div className='donateCard'>
     <img className='donateGif' src={suatmm} />
-    <div className='izi--gap'>
-      <DonatePill coin='bitcoin' address='1F67ofBSixvPt9A5kcGKSnk4ZGYXpUSwmk' />
-      <DonatePill coin='ethereum' address='0x7571FfD6D59A275FEda3a7AA5e6503Fa22E91CF8' />
-      <DonatePill coin='bitcoin cash' address='1F67ofBSixvPt9A5kcGKSnk4ZGYXpUSwmk' />
+    <br />
+    <div className='yandexCard'>
+      <iframe src='https://money.yandex.ru/quickpay/shop-widget?writer=seller&targets=To%20support%20Pathephone%20development&targets-hint=&default-sum=&button-text=14&payment-type-choice=on&hint=&successURL=&quickpay=shop&account=410015891281482' width='423' height='226' frameBorder='0' allowTransparency='true' scrolling='no' />
     </div>
+    <table className='cryptoTable'>
+      <tr>
+        <td><b>Bitcoin</b></td>
+        <td>1F67ofBSixvPt9A5kcGKSnk4ZGYXpUSwmk</td>
+      </tr>
+      <tr>
+        <td><b>Ethereum</b></td>
+        <td>0x7571FfD6D59A275FEda3a7AA5e6503Fa22E91CF8</td>
+      </tr>
+      <tr>
+        <td><b>Bitcoin Cache</b></td>
+        <td>1F67ofBSixvPt9A5kcGKSnk4ZGYXpUSwmk</td>
+      </tr>
+    </table>
   </div>
 )
 
