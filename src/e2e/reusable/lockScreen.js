c5ea42b73bd5e2dbe1159fa0488a10fd643f8ec6
Максим Ильин <negamaxi@gmail.com>
2018-07-05 17:32:05 +0300

    test(): add 500ms timeout after lock screen disappears

--- src/e2e/reusable/lockScreen.js
+++ src/e2e/reusable/lockScreen.js
@@ -1,8 +1,9 @@
 import { E2E_LOCK_SCREEN_ID } from '~data/e2eConstants'
 
-export function lockScreenWaitForNotExists () {
-  return this.app.client.waitUntil(async () => {
+export async function lockScreenWaitForNotExists () {
+  await this.app.client.waitUntil(async () => {
     const isExisting = await this.app.client.isExisting(E2E_LOCK_SCREEN_ID)
     return !isExisting
   }, 30000)
+  await new Promise(resolve => setTimeout(resolve, 500))
 }
