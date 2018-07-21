b4c9a7481a0295b0e1db74295f2afc9e4f0951f7
Максим Ильин <negamaxi@gmail.com>
2018-07-21 16:02:08 +0300

    build: bring accurate userData folder name in development mode

--- src/main/methods/withEnvironment.js
+++ src/main/methods/withEnvironment.js
@@ -1,17 +1,18 @@
 import { app } from 'electron'
 import jetpack from 'fs-jetpack'
+import path from 'path'
 
 import { ENVIRONMENT, IS_PRODUCTION, IS_TESTING, IS_DEVELOPMENT } from '#config'
 
 const withEnvronment = () => {
-  if (!IS_PRODUCTION) {
-    const userDataPath = app.getPath('userData')
-    const nextDataPath = `${userDataPath} (${ENVIRONMENT})`
-    if (IS_TESTING) {
-      jetpack.remove(nextDataPath)
-    }
-    app.setPath('userData', nextDataPath)
+  const userDataPath = app.getPath('userData')
+  const nextDataPath = path.resolve(
+    userDataPath, `../Pathephone${!IS_PRODUCTION ? ` (${ENVIRONMENT})` : ''}`
+  )
+  if (IS_TESTING) {
+    jetpack.remove(nextDataPath)
   }
+  app.setPath('userData', nextDataPath)
 
   if (IS_DEVELOPMENT) {
     app.commandLine.appendSwitch('remote-debugging-port', '9223')
