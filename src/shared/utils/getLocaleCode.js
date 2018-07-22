5381a8140bb0e073a131f0daa91321cac876ee0a
Максим Ильин <negamaxi@gmail.com>
2018-07-22 14:31:53 +0300

    fix: replace custom tracklist errors with ajv native errors

---
+++ src/shared/utils/getLocaleCode.js
@@ -0,0 +1,13 @@
+import { IS_PRODUCTION } from '#config'
+
+const getLocaleCode = () => {
+  let code = 'en'
+  if (IS_PRODUCTION) {
+    if (navigator && navigator.language.startsWith('ru')) {
+      code = 'ru'
+    }
+  }
+  return code
+}
+
+export default getLocaleCode
