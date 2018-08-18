f02838a44aeead375917fc76116eae52aed8294b
Максим Ильин <negamaxi@gmail.com>
2018-08-18 17:17:00 +0300

    fix: pick random swarn ports to avoid conflicts between ipfs daemons

--- src/shared/data/defaultIPFSDaemonConfig.js
+++ src/shared/data/defaultIPFSDaemonConfig.js
@@ -4,8 +4,8 @@ const defaultIPFSDaemonConfig = {
     API: '/ip4/127.0.0.1/tcp/0',
     Gateway: '/ip4/127.0.0.1/tcp/0',
     Swarm: [
-      '/ip4/0.0.0.0/tcp/4001',
-      '/ip6/::/tcp/4001',
+      '/ip4/0.0.0.0/tcp/0',
+      '/ip6/::/tcp/0',
     ],
   },
 };
