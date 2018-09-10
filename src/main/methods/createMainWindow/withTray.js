1a23fc1b9c766e9fa8f9e56b68be76b0ac2f907b
negamaxi <negamaxi@gmail.com>
2018-09-10 22:46:07 +0300

    add template icon with padding

--- src/main/methods/createMainWindow/withTray.js
+++ src/main/methods/createMainWindow/withTray.js
@@ -6,11 +6,14 @@ import {
 
 export default (mainWindow) => {
   let trayIconPath;
-  if (IS_MAC || IS_LINUX) {
+  if (IS_LINUX) {
     trayIconPath = `${RESOURCES_PATH}/indicator_icons/icon16x16.png`;
   }
   if (IS_WINDOWS) {
     trayIconPath = `${RESOURCES_PATH}/indicator_icons/icon16x16@2x.png`;
+  }
+  if (IS_MAC) {
+    trayIconPath = `${RESOURCES_PATH}/indicator_icons/icon16x16Template.png`;
   }
   const tray = new Tray(trayIconPath);
 
