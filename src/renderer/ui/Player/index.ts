9c540d75f8a4f213d67bfd1618666f6cce23fcb7
negamaxi <negamaxi@gmail.com>
2018-09-10 21:52:36 +0300

    fix linter errors

--- src/renderer/ui/Player/index.ts
+++ src/renderer/ui/Player/index.ts
@@ -1,13 +1,11 @@
 // tslint:disable no-relative-imports
 
 import * as playerEvents from './actions/events';
-import * as playerSelectors from './state/selectors';
 
 import './styles/styles.css';
 
 export {
-  playerEvents,
-  playerSelectors
+  playerEvents
 };
 
 export { Player as default }  from './view/Player';
